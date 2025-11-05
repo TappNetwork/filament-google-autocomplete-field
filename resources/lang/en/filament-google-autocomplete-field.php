6773b97b8f77131b4fd81931b59684d89d150b13
CSH-Tech <x600555@gmail.com>
2025-11-05 13:13:02 +0200

    Add localization for autocomplete fields

--- resources/lang/en/filament-google-autocomplete-field.php
+++ resources/lang/en/filament-google-autocomplete-field.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'autocomplete.label' => 'Address',
+    'autocomplete.placeholder' => 'Search for an address...',
     'autocomplete.searching.message' => 'Searching location on Google...',
     'autocomplete.search.prompt' => 'Type a location to search using Google',
 
