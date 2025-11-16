15836e563c2a1966f8baedacc73583a8df0fd127
Andréia Bohner <andreiabohner@gmail.com>
2025-11-15 22:33:41 -0300

    Add localization to label and placeholder

--- resources/lang/he/filament-google-autocomplete-field.php
+++ resources/lang/he/filament-google-autocomplete-field.php
@@ -2,6 +2,8 @@
 
 return [
 
+    'autocomplete.label' => 'כתובת',
+    'autocomplete.placeholder' => 'הקלד כתובת לחיפוש...',
     'autocomplete.searching.message' => 'מחפש מיקום בגוגל...',
     'autocomplete.search.prompt' => 'הקלד מיקום לחיפוש באמצעות גוגל',
 
